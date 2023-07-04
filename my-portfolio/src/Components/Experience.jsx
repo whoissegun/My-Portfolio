c33f511facaec8a08d774585b17fbadd9f2f6e1d
whoissegun <jojololadivine05@gmail.com>
2023-07-04 00:59:58 -0400

    Update Experience.jsx

--- my-portfolio/src/Components/Experience.jsx
+++ my-portfolio/src/Components/Experience.jsx
@@ -1,16 +1,24 @@
 const experiences = [
     {
         id: 1,
+        title: "Frontend Web Developer",
+        company: "Khoomi",
+        duration: "July 2023 - Present",
+        description: "Develop user-friendly using Tailwind, TypeScript, and Next.js. Utilize Google Analytics to gather valuable user behavior data, enabling data-driven optimizations that result in a significant 15% increase in conversion rates. Strategically distribute the workload between Next.js API endpoints and backend API endpoints, resulting in a remarkable 30% decrease in response time. This approach ensures optimal performance for users while preventing backend overload."
+    },
+    
+    {
+        id: 2,
         title: "Summer Admin Student",
         company: "Fasken Martineau Dumoulin LLP",
         duration: "June 2023 - Present",
         description: "Manage and maintain the company's websites across five countries using HTML, CSS, and JavaScript. Ensure WCAG 2.0 guidelines compliance leading to a 50% increase in site accessibility. Perform routine bug fixes for seamless user experience and collaborated with teams for adherence to accessibility standards."
     },
     {
-        id: 2,
+        id: 3,
         title: "Web Developer",
         company: "Blue W",
-        duration: "July 2022 - Aug 2022",
+        duration: "Feb 2023 - May 2023",
         description: "Integrated new APIs and endpoints using Django Rest Framework for efficient data exchange, resulting in faster page loads. Conducted user experience research and optimized the UI with React and Tailwind. Analyzed spatial data using PostGIS and PostgreSQL to inform expansion strategy. Created data-driven reports with Pandas and Numpy."
     },
     {
